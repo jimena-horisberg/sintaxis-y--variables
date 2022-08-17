6fe22b49cade0fbd580635b8f96abbdb3202fff6
Jimena Horisberger <jime.horis@gmail.com>
2022-08-16 23:38:43 -0300

    javascript

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+    let nombre = prompt ("Ingrese su nombre");
+    let fecha = parseInt (prompt ("Ingrese su año de nacimiento"));
+
+    let año = 2022;
+    let nacimiento = año - fecha;
+
+    let mensaje = `Hola ${nombre} tienes ${nacimiento} años de edad`;
+    alert (mensaje);
+ 
+    console.log (mensaje);
+
+    
+
+    
+  
+    
